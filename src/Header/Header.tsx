e3ffd157d81052fc76cb4d0455b11e41dcee469a
Fabricio <discordmilgrau444@gmail.com>
2025-11-01 14:59:33 -0300

    atualizações

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { IconAsset } from '@tabler/icons-react';
+import { Avatar } from '@mantine/core';
+import { IconAsset, IconBell, IconSettings } from '@tabler/icons-react';
 
 const Header = () => {
    return <div className="w-full bg-black px-6 text-white h-28 flex justify-between items-center">
@@ -6,14 +7,19 @@ const Header = () => {
         <IconAsset className="h-10 w-10" stroke={1.25} />
         <div className="text-3xl font-semibold">iJobs</div>
     </div>
-    <div>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+    <div className="flex gap-2 ">
+        <a href="/#">Find a Job</a>
+        <a href="/#">Find a Talent</a>
+        <a href="/#">Upload Job</a>
+        <a href="/#">About us</a>
     </div>
-    <div>
-        profile
+    <div className="flex gap-5 items-center">
+        <IconBell/>
+        <div className="flex items-center gap-2">
+             <div>Marshal</div>
+              <Avatar src="avatar.png" alt="it's me" />
+        </div>
+        <IconSettings/>
     </div>    
 
    </div>
